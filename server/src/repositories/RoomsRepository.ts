51e27e4fbc025b0513882e3b7e224b02243b24b9
77-aguarosada <luisrosada2018@gmail.com>
2021-03-06 20:50:26 -0300

    created Rooms Repository

---
+++ server/src/repositories/RoomsRepository.ts
@@ -0,0 +1,10 @@
+import { EntityRepository, Repository } from "typeorm";
+import {Room} from "../models/Room";
+
+@EntityRepository(Room)
+class  RoomsRepository extends Repository<Room> {
+
+
+}
+
+export {RoomsRepository}
