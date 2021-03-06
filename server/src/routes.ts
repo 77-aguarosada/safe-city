b5b966a9008a535c46a1cf8df0a39955a7d70e87
77-aguarosada <luisrosada2018@gmail.com>
2021-03-06 16:55:11 -0300

    upadate router added router student

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,15 @@
 
 import {Router} from 'express';
+import { StudentController } from './controllers/StudentController';
 
 
 
 const router = Router();
 
+const studentController = new StudentController()
 
-router.get("/users",)
+
+router.get("/students",studentController.create)
 
 
 export {router}
