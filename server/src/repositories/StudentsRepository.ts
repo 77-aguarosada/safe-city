204f929edb407b4059e6e02f9bd1285234c780be
77-aguarosada <luisrosada2018@gmail.com>
2021-03-06 16:22:35 -0300

    added students repository

---
+++ server/src/repositories/StudentsRepository.ts
@@ -0,0 +1,10 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Student } from "../models/Student";
+
+@EntityRepository(Student)
+class  StudentsRepository extends Repository<Student> {
+
+
+}
+
+export {StudentsRepository}
