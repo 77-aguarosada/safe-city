4cc4504f9d0cb3a974d490c246f206ca6768f156
77-aguarosada <luisrosada2018@gmail.com>
2021-03-06 21:45:04 -0300

    added routes for room

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 
 import {Router} from 'express';
+import { RoomController } from './controllers/RoomController';
 import { StudentController } from './controllers/StudentController';
 
 
@@ -16,5 +17,8 @@ router.get("/students",studentController.show)
 router.put("/students/:id",studentController.update)
 
 router.delete("/students/:id",studentController.delete)
+/*/**************************************************/
 
+const roomController = new RoomController()
+router.post("/students/rooms", roomController.create)
 export {router}
