5b35bd58d425689c87a4b502d9dd250529ca40ee
77-aguarosada <luisrosada2018@gmail.com>
2021-03-06 22:35:06 -0300

    controller for country

---
+++ server/src/controllers/CountryController.ts
@@ -0,0 +1,36 @@
+import { Response , Request} from "express"
+import { getCustomRepository } from "typeorm";
+import { CountriesRepository } from "../repositories/CountriesRepository";
+
+class CountryController{
+
+    async create( request:Request, response:Response){
+        const { countryStudent} = request.body;
+
+        const countryRepository = getCustomRepository(CountriesRepository);
+        const stundAlreadyExists = await countryRepository.findOne({
+            countryStudent,
+        })
+
+        if(stundAlreadyExists){
+            return response.status(400).json({
+                error:"Country already exits",
+            })
+        
+        }
+
+        const CountryStudenty = countryRepository .create({
+            countryStudent,
+        })
+        
+        await countryRepository.save(CountryStudenty)
+
+        return response.send(CountryStudenty);
+
+
+    }
+
+
+}
+
+export {CountryController}
