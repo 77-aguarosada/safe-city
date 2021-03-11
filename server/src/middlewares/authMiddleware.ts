d4b60686c604bd2ef3c391ef80a43a6ca35c3c28
77-aguarosada <luisrosada2018@gmail.com>
2021-03-11 15:23:12 -0300

    created middlewares for manager

---
+++ server/src/middlewares/authMiddleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken'
+
+class Auth{
+  async authMiddleware(request:Request, response:Response, next: NextFunction){
+
+    const {authorization} = request.headers;
+    
+    if(!authorization){
+        return response.sendStatus(401);
+    }
+
+    const token = authorization.replace('Bearer','').trim();
+
+    try{
+         const data =  jwt.verify(token,'secret')
+         console.log(data)
+    }catch{
+        return response.sendStatus(401);
+    }
+  }
+}
+
+export {Auth}
