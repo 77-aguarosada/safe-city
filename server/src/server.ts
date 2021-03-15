a3b11644e5e5b4adf7fd39d666053cdfc053e6e0
77-aguarosada <luisrosada2018@gmail.com>
2021-03-15 13:27:37 -0300

    update for cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata"
 import express from "express";
+import cors from "cors"
 
 const app = express();
 import './database'
@@ -7,8 +8,9 @@ app.use(express.json());
 
 import { router } from "./routes";
 
-
+app.use(cors())
 app.use(router)
+
 
 app.listen(3333,()=>{
     console.log("server is runing");
